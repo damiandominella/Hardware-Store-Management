acf0fb6b1330fb8837a684279436575c049e3b6d
damiandominella <damiandominella@gmail.com>
2017-09-04 16:25:52 +0200

    last fix

--- data_generator.c
+++ data_generator.c
@@ -6,6 +6,9 @@
 #define INPUT_FILE "input.txt"
 #define ARRAY_LENGTH 10
 
+void clear_buffer();
+int get_valid_int(char *field_name);
+
 int main() {
 
 	/* Defining some constant values */
@@ -18,14 +21,14 @@ int main() {
 
 	int max;
 	printf("\nInsert number of articles to generate (0-10): ");
-	scanf("%d", &max);
+	max = get_valid_int("Num articles to generate");
 
 	FILE *f = fopen(INPUT_FILE, "w");
 	if (f != NULL)
 	{
 		/* Loop until max value */
 		for (i = 0; i < max; i++) {
-			fprintf(f,"%s %s %.2f %d\n", names[i], ids[i], prices[i], quantities[i]); /* Writing to file */
+			fprintf(f, "%s %s %.2f %d\n", names[i], ids[i], prices[i], quantities[i]); /* Writing to file */
 		}
 		fclose(f);
 	} else {
@@ -36,4 +39,31 @@ int main() {
 	printf("\n%d articles generated!\n\n", max);
 
 	return 0;
+}
+
+void clear_buffer()
+{
+	char c;
+	while ((c = getchar()) != '\n' && c != EOF);
+}
+
+int get_valid_int(char *field_name)
+{
+	int is_valid = 0;
+	int value;
+	char term;
+	while (!is_valid) {
+		if (scanf("%d%c", &value, &term) != 2 || term != '\n') {
+			clear_buffer();
+			printf("%s must be an integer, try again: ", field_name);
+		} else {
+			if (value < 0 || value > ARRAY_LENGTH) {
+				printf("%s must be between 0 and 10, try again: ", field_name);
+			} else {
+				is_valid = 1;
+			}
+		}
+	}
+
+	return value;
 }
